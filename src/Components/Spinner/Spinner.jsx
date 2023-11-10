07f277cb7349ebc5df2fbb567a6e5890425327a0
AlarconCode <julian.alarcon81@gmail.com>
2023-11-10 21:11:12 +0100

    add spinner con css.module

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -1,42 +1,43 @@
-import styled from "styled-components"
+// import styled from "styled-components"
+import styles from './spinner.module.css'
 
 export const Spinner = () => {
-  return <Span className="loader"></Span>
+  return <span className={styles.loader} />
 }
 
-const Span = styled.span`
-  .loader {
-  width: 48px;
-  height: 48px;
-  border: 5px solid #FFF;
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  position: relative;
-  animation: pulse 1s linear infinite;
-}
-.loader:after {
-  content: '';
-  position: absolute;
-  width: 48px;
-  height: 48px;
-  border: 5px solid #FFF;
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  animation: scaleUp 1s linear infinite;
-}
+{/* // const Span = styled.span`
+//   .loader {
+//   width: 48px;
+//   height: 48px;
+//   border: 5px solid #FFF;
+//   border-radius: 50%;
+//   display: inline-block;
+//   box-sizing: border-box;
+//   position: relative;
+//   animation: pulse 1s linear infinite;
+// }
+// .loader:after {
+//   content: '';
+//   position: absolute;
+//   width: 48px;
+//   height: 48px;
+//   border: 5px solid #FFF;
+//   border-radius: 50%;
+//   display: inline-block;
+//   box-sizing: border-box;
+//   left: 50%;
+//   top: 50%;
+//   transform: translate(-50%, -50%);
+//   animation: scaleUp 1s linear infinite;
+// }
 
-@keyframes scaleUp {
-  0% { transform: translate(-50%, -50%) scale(0) }
-  60% , 100% { transform: translate(-50%, -50%)  scale(1)}
-}
-@keyframes pulse {
-  0% , 60% , 100%{ transform:  scale(1) }
-  80% { transform:  scale(1.2)}
-}
+// @keyframes scaleUp {
+//   0% { transform: translate(-50%, -50%) scale(0) }
+//   60% , 100% { transform: translate(-50%, -50%)  scale(1)}
+// }
+// @keyframes pulse {
+//   0% , 60% , 100%{ transform:  scale(1) }
+//   80% { transform:  scale(1.2)}
+// }
 
-`
+// ` */}
